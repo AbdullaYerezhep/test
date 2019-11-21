0cbef2078b85c50d45900affb29ef27fe7995435
wewenami <57501084+wewenami@users.noreply.github.com>
2019-11-21 15:29:45 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace home_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void bigger(int a, int b)
+            {
+                if (a < b)
+                {
+                    Console.WriteLine("Bigger one - {0}", b);
+                }
+
+                if (b < a)
+
+                {
+                    Console.WriteLine("Bigger one - {0}", a);
+                }
+
+                if (a == b)
+                {
+                    Console.WriteLine("Bigger one - {0}, but equal.", a);
+                }
+            }
+            bigger(1, 2);
+            bigger(1230, 12451);
+            bigger(1, 1);
+        }
+    }
+}
